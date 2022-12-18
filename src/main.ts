fc65b2bcca3e7fc85837d235430f286315d858ee
Hugo Richard <hrichard206@gmail.com>
2022-12-18 13:01:49 +0100

    Refacto for deploy

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
-bootstrap();
+bootstrap().then(() => console.log("Server is running on port 3000"));
