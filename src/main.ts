9c124d7b2b59bd2b243f38289e67e6f5c3834965
Hugo Richard <hrichard206@gmail.com>
2022-12-20 19:34:17 +0100

    Try to fix CORS error

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,16 @@ import * as cookieParser from "cookie-parser";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { ClassSerializerInterceptor } from "@nestjs/common";
 
-const domains = [
-  "http://localhost:8080",
-  "http://localhost:3000",
-  "https://vuejs-frontend-template.herokuapp.com",
-];
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.enableCors({
-    origin: domains,
+    origin: [
+      "http://localhost:8080",
+      "http://localhost:3000",
+      "https://vuejs-frontend-template.herokuapp.com",
+    ],
     credentials: true,
   });
   const config = new DocumentBuilder()
